321ceb4106b52e37e06e4164b7ef56fcb413c6d4
Ricardo SIGNES <rjbs@cpan.org>
2007-08-02 09:53:37 -0400

    tweaks

--- lib/UsePerl.pm
+++ lib/UsePerl.pm
@@ -15,40 +15,46 @@ my $journal = Slash::Client::Journal->new({
   pass => 'password',
 });
 
+sub rewrite_markdown {
+  my ($content) = @_;
+  $content =~ s/!\[/\[inline image:/g;
+  $content = Text::Markdown::markdown($content, { tab_width => 2 });
+  $content =~ s|<pre><code>|<ecode>|g;
+  $content =~ s|</code></pre>|</ecode>|g;
+
+  my $tb = HTML::TreeBuilder->new;
+  $tb->implicit_tags(0);
+  $tb->no_space_compacting(1);
+
+  my $root = $tb->parse($content);
+  $tb->eof;
+
+  my (@ecodes) = $root->look_down(_tag => 'ecode');
+
+  for my $ecode (@ecodes) {
+    # We're basically guaranteed that ecode has no HTML contents.
+    my $string = join '', $ecode->content_list;
+
+    $ecode->delete_content;
+    $ecode->push_content(HTML::Element->new('~literal', text => $string));
+  }
+
+  return $root->as_XML;
+}
+
 sub add_entry { shift;
   my ($arg) = @_;
 	my ($title, $content, $uri) = @$arg{qw(title body uri)};
   my $posttype = 1; # default: plain old text
+
   if ($arg->{tags}||'' =~ /\@markup:md/) {
     $content = "[$title]($uri)\n\n$content" if $uri;
-    $content =~ s/!\[/\[inline image:/g;
-    $content = Text::Markdown::markdown($content, { tab_width => 2 });
-    $content =~ s|<pre><code>|<ecode>|g;
-    $content =~ s|</code></pre>|</ecode>|g;
-
-    my $tb = HTML::TreeBuilder->new;
-    $tb->implicit_tags(0);
-    $tb->no_space_compacting(1);
-
-    my $root = $tb->parse($content);
-    $tb->eof;
-
-    my (@ecodes) = $root->look_down(_tag => 'ecode');
-
-    for my $ecode (@ecodes) {
-      # We're basically guaranteed that ecode has no HTML contents.
-      my $string = join '', $ecode->content_list;
-
-      $ecode->delete_content;
-      $ecode->push_content(HTML::Element->new('~literal', text => $string));
-    }
-
-    $content = $root->as_XML;
-
+    $content = rewrite_markdown($content);
     $posttype = 2; # html
   } else {
     $content = "$uri\n\n$content" if $uri;
   }
+
 	my $j = {
     subject  => $title,
     body     => $content,
