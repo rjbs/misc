5a0dd3fc4c9c9ce835726ab01f074c6b8e94d5e8
Ricardo Signes <rjbs@cpan.org>
2009-12-23 22:08:29 -0500

    remove crap

--- gopher.pl
+++ gopher.pl
@@ -1,87 +1,42 @@
 #!/usr/bin/perl
-
-=head1 NAME
-
-gopher - a gopher server (I'm sorry!)
-
-=cut
-
+# gopher - a gopher server (I'm sorry!)
 use strict;
+use warnings;
 use IO::Socket;
 
+my $port = 70;
 my $crlf = "\015\012";
 
-my ($bytes_out,$bytes_in) = (0,0);
-
 my $socket = IO::Socket::INET->new(
-	Proto => 'tcp', LocalPort => 70, Listen => 128, Reuse => 1
+  Proto     => 'tcp',
+  LocalPort => $port,
+  Listen    => 128,
+  Reuse     => 1
 );
 
-warn "waiting for incoming connections on port 70...\n";
-
-my $directory = { content => [
-	{ name => 'hello', desc => 'hello', content => 'Hello, 1993-era world!' },
-	{
-    name => 'wtf',   desc => "I don't get it.", content => [
-      { name => 'wtf', desc => 'What is this?', content => 'Gopher!' },
-      { name => 'why', desc => 'Why would you do this?', content => 'Cuz!' },
-    ]
-	}
-]};
+warn "waiting for incoming connections on port $port...\n";
 
 while (1) {
   next unless my $connection = $socket->accept;
   if (fork) {
-  	$connection->close;
-  	next;
+    $connection->close;
+    next;
   } else {
-		$connection->autoflush(1);
-		my $request = $connection->getline;
+    $connection->autoflush(1);
+    my $request = $connection->getline;
 
-		$request =~ s/$crlf//g;
+    $request =~ s/$crlf//g;
+    print STDERR "REQUEST: $request\n";
 
-		my $item = get_item($request);
-		unless ($item) {
-			print $connection "ERROR: request leads nowhere!$crlf";
-			print $connection ".$crlf";
-		} else {
-			print $connection format_item($item, $request);
-		}
-		
-		$connection->close;
-	}
-}
-
-sub get_item {
-	my ($index, $dir) = @_;
-	warn ">> $dir\n";
-	$dir ||= $directory;
-	my ($head, $tail) = split '/', $index, 2;
-	return $dir unless $head;
-	my $subdir;
-	if (ref($dir->{content})||'' eq 'ARRAY') {
-		$subdir = grep { $_->{name} eq $head } @{$dir->{content}};
-	} elsif (ref($dir->{content})||'' eq 'HASH') {
-		$subdir = $dir->{content}{$head};
-	}
-	if ($subdir) { return get_item($tail, $subdir) }
-	return;
-}
-
-sub format_item {
-	my ($item, $request) = @_;
-	my $leader = "$request/" if $request;
-
-	if (ref $item->{content} eq 'ARRAY') {
-		my $output;
-		foreach (@{$item->{content}}) {
-			$output .= ((ref $_->{content}) eq 'ARRAY' ? 1 : 0);
-			$output .= "$_->{desc}\t$leader$_->{name}\tknave\t70$crlf";
-		}
-		return $output;
-	} else {
-		return $item->{content};
-	}
+    if ($request eq '' or $request eq '/') {
+      $connection->print("1Directory Listing\t/\tlocalhost\t70$crlf");
+      $connection->print("0README.TXT\tREADME.TXT\tlocalhost\t70$crlf");
+      $connection->print("0README.TxT\tREADME.TxT\tlocalhost\t70$crlf");
+      $connection->print(".$crlf");
+    }
+    
+    $connection->close;
+  }
 }
 
 $socket->close;
