979d92f519359ec93173bc53ed8c644a90ecae74
Ricardo Signes <rjbs@users.noreply.github.com>
2020-11-16 14:32:07 -0500

    modulified fastamper by nick w

--- fastamper.js
+++ fastamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fastmail Shortcuts
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  mouse less, keyboard more
 // @author       rjbs
 // @match        https://*.fastmail.com/mail/*
@@ -10,140 +10,149 @@
 
 (function() {
   'use strict';
-  window.setTimeout(
-    function () {
-      const css = O.Element.create(
-        'style',
-        { type: 'text/css' },
-        [ '.rjbs-MSV-Hidden-3 .v-MailboxSource-badge { background-color: #e3d8f0 }' ],
-      );
-      document.body.appendChild(css);
+  const observer = new MutationObserver(() => {
+    if (document.getElementById('bootstrap-page')) {
+      return;
+    }
+    observer.disconnect();
 
-      const shortcut = (keystroke, fn) => {
-        FastMail.keyboardShortcuts.register(keystroke, { do: fn }, 'do');
-      };
+    const css = FM.el(
+      'style',
+      { type: 'text/css' },
+      [ '.rjbs-MSV-Hidden-3 .v-MailboxSource-badge { background-color: #e3d8f0 }' ],
+    );
+    document.body.appendChild(css);
 
-      shortcut(
-        'Cmd-Shift-L',
-        () => {
-          if (FastMail.userPrefs.get('theme') === 'steel') {
-            FastMail.userPrefs.set('theme', 'dark');
-          } else {
-            FastMail.userPrefs.set('theme', 'steel');
-          }
-        },
-      );
+    const shortcut = (keystroke, fn) => {
+      FM.ViewEventsController.kbShortcuts.register(keystroke, { do: fn }, 'do');
+    };
 
-      shortcut('Cmd-Shift-9', () => FastMail.mail.sources.get('sourceGroups')[0].content.forEach(s => s.set('isCollapsed', true)));
-      shortcut('Cmd-Shift-0', () => FastMail.mail.sources.get('sourceGroups')[0].content.forEach(s => s.set('isCollapsed', false)));
-
-      shortcut('(', () => FastMail.mail.set('mailboxFilter', ''));
-      shortcut('|', () => FastMail.mail.set('mailboxFilter', 'inbox'));
-      shortcut(')', () => FastMail.mail.set('mailboxFilter', 'unread'));
-
-      shortcut('Cmd-Shift-Z', () => FastMail.preferences.toggle('enableConversations'));
-      shortcut('Cmd-Shift-D', () => FastMail.preferences.toggle('showSidebar'));
-      shortcut('Cmd-Shift-G', () => FastMail.mail.toggle('searchIsGlobal'));
-      shortcut('Cmd-Shift-P', () => FastMail.preferences.toggle('showReadingPane'));
-
-      const clippy = {
-        text: {
-          cycle: 0,
-          range: null,
-          opts : [ '#a00', '#a0a', '#3b874b', '#d68b00', '#43219c', null ],
-        },
-        block: {
-          cycle: 0,
-          range: null,
-          opts : [
-            (e) => { e.style.backgroundColor = '#dacae0'; e.style.color = '#54365e'; },
-            (e) => { e.style.backgroundColor = '#f5fca7'; e.style.color = '#9c6500'; },
-            (e) => { e.style.backgroundColor = '#c3e0c9'; e.style.color = '#275731'; },
-            (e) => { e.style.backgroundColor = null; e.style.color = null; },
-          ],
-        },
-        nextFor: function (key, range) {
-          const state = this[key];
-          state.cycle = (state.range && (range.compareBoundaryPoints(Range.END_TO_END, state.range) == 0))
-                      ? ((state.cycle + 1) % state.opts.length)
-                      : 0;
-
-          return state.opts[ state.cycle ];
-        },
-      };
-
-      const krazyKolour = () => {
-        let editorViewElements = document.getElementsByClassName('v-RichText');
-        if (editorViewElements.length != 1) {
-          console("RJBS:  Wanted exactly one v-RichText but got " + editorViewElements.length + ".");
-          return null;
-        }
-
-        let editor = O.getViewFromNode(editorViewElements[0]).editor;
-
-        if (! editor) {
-          console.log("No editor?  I give up.");
-          return null;
-        }
-
-        const range = editor.getSelection();
-        if (range.collapsed) {
-          const quote = (range.startContainer instanceof Element)
-                      ? range.startContainer.closest('blockquote')
-                      : range.startContainer.parentElement.closest('blockquote');
-
-          if (! quote) {
-            console.log("Not inside a blockquote.");
-            return null;
-          }
-
-          const munger = clippy.nextFor('block', range);
-          munger(quote);
-          clippy.block.range = editor.getSelection();
+    shortcut(
+      'Cmd-Shift-L',
+      () => {
+        if (FM.userPrefs.get('theme') === 'steel') {
+          FM.userPrefs.set('theme', 'dark');
         } else {
-          const color = clippy.nextFor('text', range);
-
-          editor.setTextColour( color );
-          clippy.text.range = editor.getSelection();
+          FM.userPrefs.set('theme', 'steel');
         }
-      };
+      },
+    );
 
-      shortcut('Cmd-Shift-K', krazyKolour);
+    const mail = FM.router.getAppController('mail');
+    shortcut('Cmd-Shift-9', () => mail.sources.get('sourceGroups')[0].content.forEach(s => s.set('isCollapsed', true)));
+    shortcut('Cmd-Shift-0', () => mail.sources.get('sourceGroups')[0].content.forEach(s => s.set('isCollapsed', false)));
 
-      JMAP.Mailbox.prototype.badgeProperty = function () {
-        var role = this.get('role');
+    shortcut('(', () => mail.set('mailboxFilter', ''));
+    shortcut('|', () => mail.set('mailboxFilter', 'inbox'));
+    shortcut(')', () => mail.set('mailboxFilter', 'unread'));
 
-        if ( role === 'drafts' ) return 'totalEmails';
-        if ( role === 'archive' || role === 'sent' || role === 'trash' || role === 'snoozed' ) {
+    shortcut('Cmd-Shift-Z', () => FM.preferences.toggle('enableConversations'));
+    shortcut('Cmd-Shift-D', () => FM.preferences.toggle('showSidebar'));
+    shortcut('Cmd-Shift-G', () => mail.toggle('searchIsGlobal'));
+    shortcut('Cmd-Shift-P', () => FM.preferences.toggle('showReadingPane'));
+
+    const clippy = {
+      text: {
+        cycle: 0,
+        range: null,
+        opts : [ '#a00', '#a0a', '#3b874b', '#d68b00', '#43219c', null ],
+      },
+      block: {
+        cycle: 0,
+        range: null,
+        opts : [
+          (e) => { e.style.backgroundColor = '#dacae0'; e.style.color = '#54365e'; },
+          (e) => { e.style.backgroundColor = '#f5fca7'; e.style.color = '#9c6500'; },
+          (e) => { e.style.backgroundColor = '#c3e0c9'; e.style.color = '#275731'; },
+          (e) => { e.style.backgroundColor = null; e.style.color = null; },
+        ],
+      },
+      nextFor: function (key, range) {
+        const state = this[key];
+        state.cycle = (state.range && (range.compareBoundaryPoints(Range.END_TO_END, state.range) == 0))
+        ? ((state.cycle + 1) % state.opts.length)
+        : 0;
+
+        return state.opts[ state.cycle ];
+      },
+    };
+
+    const getViewsByClass = (viewClass) => {
+      return Object.values(FM.activeViews).filter(view => view instanceof viewClass);
+    }
+
+    const krazyKolour = () => {
+      let editorViews = getViewsByClass(FM.classes.RichTextView);
+      if (editorViews.length != 1) {
+        console("RJBS:  Wanted exactly one v-RichText but got " + editorViews.length + ".");
+        return null;
+      }
+
+      let editor = editorViews[0].editor;
+
+      if (! editor) {
+        console.log("No editor?  I give up.");
+        return null;
+      }
+
+      const range = editor.getSelection();
+      if (range.collapsed) {
+        const quote = (range.startContainer instanceof Element)
+        ? range.startContainer.closest('blockquote')
+        : range.startContainer.parentElement.closest('blockquote');
+
+        if (! quote) {
+          console.log("Not inside a blockquote.");
           return null;
         }
 
-        var forceEmail = this.get('isShared') && !this.get('isSeenShared');
+        const munger = clippy.nextFor('block', range);
+        munger(quote);
+        clippy.block.range = editor.getSelection();
+      } else {
+        const color = clippy.nextFor('text', range);
 
-        if ( this.get('hidden') === JMAP.Mailbox.HIDE_IF_EMPTY) {
-          return forceEmail ? 'totalEmails' : 'total';
-        }
+        editor.setTextColour( color );
+        clippy.text.range = editor.getSelection();
+      }
+    };
 
-        return forceEmail ? 'unreadEmails' : 'unread';
-      }.property( 'role', 'isShared', 'isSeenShared', 'hidden' );
+    shortcut('Cmd-Shift-K', krazyKolour);
 
-      JMAP.store.getAll(JMAP.Mailbox).forEach(mailbox => mailbox.computedPropertyDidChange('badgeProperty'));
+    FM.classes.Mailbox.prototype.badgeProperty = function () {
+      var role = this.get('role');
 
-      FastMail.MailboxSourceView.prototype.className = function () {
-        var role = this.get( 'content' ).get( 'role' );
-        var isCollapsed = !this.get( 'hasSubfolders' ) || this.get( 'isCollapsed' );
+      if ( role === 'drafts' ) return 'totalEmails';
+      if ( role === 'archive' || role === 'sent' || role === 'trash' || role === 'snoozed' ) {
+        return null;
+      }
 
-        return 'v-MailboxSource' +
-            ( role ? ' v-MailboxSource--' + role : '' ) +
-            ( ' rjbs-MSV-Hidden-' + this.get('content').get('hidden') ) +
-            ( isCollapsed ? '' : ' is-expanded' ) +
-            ( isCollapsed && this.get( 'hasUnreadChildren' ) ? ' u-bold' : '' );
-      }.property( 'hasSubfolders', 'isCollapsed', 'hasUnreadChildren' );
+      var forceEmail = this.get('isShared') && !this.get('isSeenShared');
 
-      Array.from(document.getElementsByClassName('v-MailboxSource')).forEach(
-        source => O.getViewFromNode(source).computedPropertyDidChange('className')
-      );
-    },
-    1000
-  );
+      // No easy reference to HIDE_IF_EMPTY so use hardcoded value
+      if ( this.get('hidden') === 3) {
+        return forceEmail ? 'totalEmails' : 'total';
+      }
+
+      return forceEmail ? 'unreadEmails' : 'unread';
+    }.property( 'role', 'isShared', 'isSeenShared', 'hidden' );
+
+    FM.store.getAll(FM.classes.Mailbox).forEach(mailbox => mailbox.computedPropertyDidChange('badgeProperty'));
+
+    FM.classes.MailboxSourceView.prototype.className = function () {
+      var role = this.get( 'content' ).get( 'role' );
+      var isCollapsed = !this.get( 'hasSubfolders' ) || this.get( 'isCollapsed' );
+
+      return 'v-MailboxSource' +
+      ( role ? ' v-MailboxSource--' + role : '' ) +
+      ( ' rjbs-MSV-Hidden-' + this.get('content').get('hidden') ) +
+      ( isCollapsed ? '' : ' is-expanded' ) +
+      ( isCollapsed && this.get( 'hasUnreadChildren' ) ? ' u-bold' : '' );
+    }.property( 'hasSubfolders', 'isCollapsed', 'hasUnreadChildren' );
+
+    getViewsByClass(FM.classes.MailboxSourceView).forEach(
+      view => view.computedPropertyDidChange('className')
+    );
+  });
+  observer.observe(document.body, { childList: true });
 })();
