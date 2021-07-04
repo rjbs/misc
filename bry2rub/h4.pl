b41380bef9a7b4fefb5c58ec0c83c701c00328ca
Ricardo Signes <rjbs@semiotic.systems>
2021-07-03 21:18:31 -0400

    nobody is ever converting from Bryar to Rubric again

--- bry2rub/h4.pl
+++
@@ -1,9 +0,0 @@
-use Rubric::User;
-$rjbs = Rubric::User->retrieve('rjbs');
-for my $entry ($rjbs->entries) {
-	next unless $entry->body =~ /h4/;
-	$body = $entry->body;
-	$body =~ s/\s*(<h\d>(?:[^<]+)<\/h\d>)\s*/$1/g;
-	$entry->body($body);
-	$entry->update;
-}
